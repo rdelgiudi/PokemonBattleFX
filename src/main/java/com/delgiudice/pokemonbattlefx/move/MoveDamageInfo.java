c267c2f98b2e6262206e8306070abfacd52ce758
Roberto <rdelgiudi@gmail.com>
2024-01-21 19:41:26 +0100

    MoveDamageInfo Javadoc comments complete

--- src/main/java/com/delgiudice/pokemonbattlefx/move/MoveDamageInfo.java
+++ src/main/java/com/delgiudice/pokemonbattlefx/move/MoveDamageInfo.java
@@ -1,9 +1,25 @@
 package com.delgiudice.pokemonbattlefx.move;
 
+import com.delgiudice.pokemonbattlefx.attributes.Type;
+import com.delgiudice.pokemonbattlefx.battle.BattleLogic;
+
+/**
+ * Data container class which holds information about calculated move damage and other information.
+ */
 public class MoveDamageInfo {
     public static final int NOT_APPLICABLE = -1;
+    /**
+     * Damage dealt.
+     */
     public int damage;
+    /**
+     * If the move was a critical hit, set this value to <code>true</code>.
+     */
     public boolean critical;
+    /**
+     * Type effect multiplier.
+     * @see BattleLogic#calculateTypeEffect(Move, Type, Type)
+     */
     public float typeEffect;
 
     public MoveDamageInfo(int damage, boolean critical, float typeEffect) {
